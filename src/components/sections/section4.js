96437ae34e6392d755977b3ec92c9f77d9781b33
maxwell <maxwellmichael522@gmil.com>
2022-06-13 00:21:14 +0530

    Fixed a warning

--- src/components/sections/section4.js
+++ src/components/sections/section4.js
@@ -3,7 +3,6 @@ import { useForm } from 'react-hook-form'
 import emailjs from '@emailjs/browser'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
-import Popup from 'reactjs-popup'
 
 
 import {
